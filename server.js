8bc6bf2a26593eed5462f9d498561db402f870e5
lwebb21 <47036903+lwebb21@users.noreply.github.com>
2019-03-17 16:29:24 -0600

    Update

--- server.js
+++ server.js
@@ -88,25 +88,17 @@ app.get('/', function(req, res) {
 	});
 });
 
-// registration page 
-app.get('/register', function(req, res) {
-	res.render('pages/register',{
-		my_title:"Registration Page"
-	});
-});
-
-/*Add your other get/post request handlers below here: */
-
 app.get('/home', function(req, res) {
-	var query = 'select * from favorite_colors;';
-	db.any(query)
+  var query = 'select * from favorite_colors;';
+  db.any(query)
         .then(function (rows) {
+        //console.log(info);
             res.render('pages/home',{
-				my_title: "Home Page",
-				data: rows,
-				color: '',
-				color_msg: ''
-			})
+        my_title: "Home Page",
+        data: rows,
+        color: '',
+        color_msg: ''
+      })
 
         })
         .catch(function (err) {
@@ -120,24 +112,23 @@ app.get('/home', function(req, res) {
             })
         })
 });
-
 app.get('/home/pick_color', function(req, res) {
-	var color_choice = req.query.color_selection;
-	var color_options =  'select * from favorite_colors;';
-	var color_message = "select color_msg from favorite_colors where hex_value = '" + color_choice + "';"; 
-	db.task('get-everything', task => {
+  var color_choice = req.query.color_selection;
+  var color_options =  'select * from favorite_colors;';
+  var color_message = "select color_msg from favorite_colors where hex_value = '" + color_choice + "';"; 
+  db.task('get-everything', task => {
         return task.batch([
             task.any(color_options),
             task.any(color_message)
         ]);
     })
     .then(info => {
-    	res.render('pages/home',{
-				my_title: "Home Page",
-				data: info[0],
-				color: color_choice,
-				color_msg: info[1][0].color_msg
-			})
+      res.render('pages/home',{
+        my_title: "Home Page",
+        data: info[0],
+        color: color_choice,
+        color_msg: info[1][0].color_msg
+      })
     })
     .catch(error => {
         // display error message in case an error
@@ -149,30 +140,30 @@ app.get('/home/pick_color', function(req, res) {
                 color_msg: ''
             })
     });
-	
+  
 });
 
 app.post('/home/pick_color', function(req, res) {
-	var color_hex = req.body.color_hex;
-	var color_name = req.body.color_name;
-	var color_message = req.body.color_message;
-	var insert_statement = "INSERT INTO favorite_colors(hex_value, name, color_msg) VALUES('" + color_hex + "','" + 
-							color_name + "','" + color_message +"') ON CONFLICT DO NOTHING;";
+  var color_hex = req.body.color_hex;
+  var color_name = req.body.color_name;
+  var color_message = req.body.color_message;
+  var insert_statement = "INSERT INTO favorite_colors(hex_value, name, color_msg) VALUES('" + color_hex + "','" + 
+              color_name + "','" + color_message +"') ON CONFLICT DO NOTHING;";
 
-	var color_select = 'select * from favorite_colors;';
-	db.task('get-everything', task => {
+  var color_select = 'select * from favorite_colors;';
+  db.task('get-everything', task => {
         return task.batch([
             task.any(insert_statement),
             task.any(color_select)
         ]);
     })
     .then(info => {
-    	res.render('pages/home',{
-				my_title: "Home Page",
-				data: info[1],
-				color: color_hex,
-				color_msg: color_message
-			})
+      res.render('pages/home',{
+        my_title: "Home Page",
+        data: info[1],
+        color: color_hex,
+        color_msg: color_message
+      })
     })
     .catch(error => {
         // display error message in case an error
@@ -186,6 +177,15 @@ app.post('/home/pick_color', function(req, res) {
     });
 });
 
+
+// registration page 
+app.get('/register', function(req, res) {
+	res.render('pages/register',{
+		my_title:"Registration Page"
+	});
+});
+
+/*Add your other get/post request handlers below here: */
 app.get('/team_stats', function(req, res)
 {
   var query = 'select * from football_games;';
